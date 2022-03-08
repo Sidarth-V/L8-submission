8f55f01977d32a1f9f7a25d9fbb812eb3221243f
Sidarth V <sidarth.v14002@gmail.com>
2022-03-08 14:24:12 +0530

    add: models

--- tasks/models.py
+++ tasks/models.py
@@ -3,13 +3,35 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+STATUS_CHOICES = (
+    ("PENDING", "PENDING"),
+    ("IN_PROGRESS", "IN_PROGRESS"),
+    ("COMPLETED", "COMPLETED"),
+    ("CANCELLED", "CANCELLED"),
+)
+
 class Task(models.Model):
+    _old_status = None
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     completed = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
-    user = models.ForeignKey(User , on_delete=models.CASCADE , null=True,blank=True)
+    priority = models.IntegerField(null=True, blank=True)
+    status = models.CharField(
+        max_length=100, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0]
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
-    def __str__(self):
-        return self.title
+    def __init__(self,  *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._old_status = self.status
+
+
+class TaskHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    old_status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+    new_status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+    time_of_change = models.DateTimeField(auto_now=True)
